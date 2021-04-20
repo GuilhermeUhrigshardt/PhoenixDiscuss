ee431c6e4775ad14596b1d5489cfa559fca60549
GuilhermeUhrigshardt <guilhermeuh@gmail.com>
2021-04-20 19:28:38 -0300

    sign out option

---
+++ lib/discuss_web/plugs/set_user.ex
@@ -0,0 +1,19 @@
+defmodule DiscussWeb.Plugs.SetUser do
+  import Plug.Conn
+
+  alias Discuss.Users
+
+  def init(_params) do
+  end
+
+  def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = user_id && Users.get_user!(user_id) ->
+        assign(conn, :user, user)
+      true ->
+        assign(conn, :user, nil)
+    end
+  end
+end
